c1eb75abc261fb9f62b6fbc5beb2455d9937fae4
mshakes <marcusshakes@gmail.com>
2018-11-27 21:29:31 +0000

    Second Ever Commit
    Added searching for a booklet

--- demo.rb
+++ demo.rb
@@ -1,5 +1,7 @@
 require "selenium-webdriver"
 driver = Selenium::WebDriver.for :chrome
 driver.navigate.to "https://www.bhf.org.uk/informationsupport/publications"
+driver.find_element(:id, 'main_0_pagecontent_0_uxKeyword').send_keys' stroke'
+driver.find_element(:id, 'main_0_pagecontent_0_uxPubSearchBtn').click
 puts driver.title
 driver.quit
