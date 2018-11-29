549d99819a37ad9ff9ed8d7a0bf88100e115a2dd
mshakes <marcusshakes@gmail.com>
2018-11-29 22:18:07 +0000

    small tweaks and order change

--- demo.rb
+++ demo.rb
@@ -3,9 +3,8 @@ require 'watir-scroll'
 require 'watir'
 browser = Watir::Browser.new
 browser.goto "https://www.bhf.org.uk/informationsupport/publications"
+browser.button(:css =>".g-cookies__cta button").click
 browser.text_field(:id => "main_0_pagecontent_0_uxKeyword").set "stroke"
 browser.button(:id => "main_0_pagecontent_0_uxPubSearchBtn").click
-browser.button(:css =>".g-cookies__cta button").click
 browser.scroll.to :center
-
-
+browser.button(:xpath => "/html/body/form/div[3]/section/div[5]/div[2]/div[2]/div/div[2]/input[2]").click
