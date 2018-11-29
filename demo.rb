c6277d1bc862da81db9701391a78ff9e13ad62c0
mshakes <marcusshakes@gmail.com>
2018-11-29 23:12:52 +0000

    small update
    progress!

--- demo.rb
+++ demo.rb
@@ -8,3 +8,4 @@ browser.text_field(:id => "main_0_pagecontent_0_uxKeyword").set "stroke"
 browser.button(:id => "main_0_pagecontent_0_uxPubSearchBtn").click
 browser.scroll.to :center
 browser.button(:xpath => "/html/body/form/div[3]/section/div[5]/div[2]/div[2]/div/div[2]/input[2]").click
+browser.button(:id => "main_0_pagecontent_0_btnCheckkout").click 
